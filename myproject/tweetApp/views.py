f2272e02ec2f122173ea85932309e129dd8ade41
javierfm27 <javitofm27@github.com>
2017-05-03 12:26:24 +0200

    Inacabada

---
+++ myproject/tweetApp/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import *
+from django.views.decorators.csrf import csrf_exempt
+from urllib.request import urlopen
+import feedparser
+
+def obtainTitle(canal):
+    titles = []
+    for i in range(5):
+        titles.append(canal.entries[i].title)
+    return titles
+# Create your views here.
+@csrf_exempt
+def barra(request):
+    if request.method == 'GET':
+        bodyHtml = "Introduce el User del que quieras obtener sus 5 ultimos tweets<br>" \
+            + "<form method='POST'>\n <input type='text' name='user'></form>"
+        return HttpResponse(bodyHtml)
+    elif request.method == 'POST':
+        user = request.POST['user']
+        url = "http://" + request.META['HTTP_HOST'] + "/feed/" + user
+        return HttpResponseRedirect(url)
+
+
+def feedUser(request, user):
+    urlFeed = "https://twitrss.me/twitter_user_to_rss/?user="
+    urlFeed = urlFeed + user
+    stringRSS = urlopen(urlFeed)
+    canalRSS = feedparser.parse(stringRSS) #Cada item es una entrada, asique para obtener 5 basta con hacer un bucle
+    #Primero obtenemos el tuit
+    twets = obtainTitle(canalRSS)
+    print(twets)
+    return HttpResponse(twets)
